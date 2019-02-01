3cdf956fadf399f6248b68feff773e854772037b
DrakirL <rikard.nt.larsson@hotmail.com>
2019-02-01 10:41:45 +0100

    Added Mostly empty scripts as preparation.

---
+++ Unity/Assets/The Game/Scripts/Player_Walk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Walk : MonoBehaviour
+{
+    //Controls
+    public string horizontalAxis;
+
+    //Variables
+    public float speed;
+
+    //Componenets
+    Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComponentSetup();
+    }
+
+    void ComponentSetup()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.velocity = new Vector2(Input.GetAxis(horizontalAxis) * speed, rb2D.velocity.y);
+    }
+}
