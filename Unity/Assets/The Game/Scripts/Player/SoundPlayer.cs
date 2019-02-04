a1140fa2761ed48aa893dbe3ba2a0db1ed941839
DrakirL <rikard.nt.larsson@hotmail.com>
2019-02-04 15:31:16 +0100

    -modified file structure

---
+++ Unity/Assets/The Game/Scripts/Player/SoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource musicSource;
+    AudioSource effectSource;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
