0d8684cd1ab2d03c2d2c0aba71cedbec1f76ea56
DrakirL <rikard.nt.larsson@hotmail.com>
2019-02-04 15:31:16 +0100

    -modified file structure

---
+++ Unity/Assets/The Game/Scripts/LevelExit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    MenuMaster menuMaster;
+
+    public static event System.Action OnExitStart;
+    public static event System.Action OnExitEnd;
+
+    [SerializeField]
+    string targetScene;
+
+    [SerializeField]
+    float exitDelay;
+
+    private void Start()
+    {
+        menuMaster = GetComponent<MenuMaster>();
+    }
+
+    public void Exit()
+    {
+        OnExitStart?.Invoke();
+        Invoke("NextLevel", exitDelay);
+
+    }
+
+    void NextLevel()
+    {
+        menuMaster.ChangeScene(targetScene);
+        OnExitEnd?.Invoke();
+
+    }
+}
