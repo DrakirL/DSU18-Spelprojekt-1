0ffe91c2dbad3bcdcdd2dc2e27ccdf29067901d6
DrakirL <rikard.nt.larsson@hotmail.com>
2019-02-11 10:12:45 +0100

    Added level enabler/disabler

---
+++ Unity/Assets/LevelEnabler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnabler : MonoBehaviour
+{
+    void Awake()
+    {
+        Camera.main.GetComponent<CameraMove>().OnLevelEnter += EnableLevel;
+    }
+
+    void EnableLevel(Transform room)
+    {
+        gameObject.SetActive(transform.parent == room);
+    }
+}
--- Unity/Assets/The Game/Scripts/Gameobjects/LevelEntranceSnap.cs
+++ Unity/Assets/The Game/Scripts/Gameobjects/LevelEntranceSnap.cs
@@ -13,19 +13,4 @@ public class LevelEntranceSnap : LevelEntrance
         GameObject.Find("World").GetComponent<WorldSpin>().SnapRotation(directionFromEnum(StartingDirection));
         PlayerPrefs.SetString("SavedRoom", transform.parent.transform.name);
     }
-
-    Vector2 directionFromEnum(Direction dir)
-    {
-        if (dir == Direction.DOWN)
-            return Vector2.down;
-
-        else if (dir == Direction.UP)
-            return Vector2.up;
-
-        else if (dir == Direction.LEFT)
-            return Vector2.left;
-
-        else
-            return Vector2.right;
-    }
 }
