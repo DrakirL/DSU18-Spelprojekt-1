9bcbe16dddc7724b1dd588097b5ca9604e65bec5
ErikLNH <erikh99@gmail.com>
2019-02-12 16:27:57 +0100

    Interactions/Win
    Added win
    Added lever

---
+++ Unity/Assets/The Game/Scripts/Gameobjects/InteractableLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableLock : Interactable
+{
+    public int prerequisites;
+    public int prerequisitesMet;
+    
+    public override void Interact(GameObject obj)
+    {
+        if (prerequisitesMet >= prerequisites)
+        {
+        base.Interact(obj);
+        }
+    }
+
+}
