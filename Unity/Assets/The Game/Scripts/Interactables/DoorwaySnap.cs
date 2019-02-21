9526205e1a79692c3ff3af19cb35fb702b32ca75
DrakirL <rikard.nt.larsson@hotmail.com>
2019-02-21 16:21:30 +0100

    Sorted a lot.

---
+++ Unity/Assets/The Game/Scripts/Interactables/DoorwaySnap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorwaySnap : Doorway
+{
+
+    public override void ExitRoom()
+    {
+        Vector2 currentDown = GameObject.Find("World").transform.rotation * Vector2.down;
+        var orientation = directionFromEnum(Orientation);
+
+        if (orientation != currentDown)
+            return;
+
+        base.ExitRoom();
+    }
+
+    protected override void StartRoom(Transform newRoom)
+    {
+        if (Room != newRoom)
+            return;
+
+        
+        var newDir = directionFromEnum(Orientation);
+
+
+        GameObject.Find("Player").transform.position = transform.position;
+
+        GameObject.Find("World").GetComponent<WorldSpin>().SnapRotation(newDir);
+
+        PlayerPrefs.SetString("SavedRoom", transform.parent.transform.name);
+    }
+}
