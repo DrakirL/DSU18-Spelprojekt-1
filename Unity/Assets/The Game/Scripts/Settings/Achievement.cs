d4c3957cfd5882579ef8e481ade97a487c6e88e0
DrakirL <rikard.nt.larsson@hotmail.com>
2019-02-26 15:10:28 +0100

    OnFirstInteract()

---
+++ Unity/Assets/The Game/Scripts/Settings/Achievement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievement : MonoBehaviour
+{
+    int deathCount;
+    int flipCount;
+    float playStartTime;
+
+    int objectsFound;
+
+    public void IncreaseDeathCount() => deathCount++;
+    public void IncreaseFlipCount() => flipCount++;
+    public void IncreaseObjectsFound() => objectsFound++;
+
+    private void Awake()
+    {
+        var death = GameObject.Find("Player").GetComponent<Player_Death>();
+        
+
+        death.AfterDie += IncreaseDeathCount;
+    }
+
+    public void Reset()
+    {
+        deathCount = flipCount = objectsFound = 0;
+        playStartTime = Time.time;
+    }
+}
