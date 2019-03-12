83e9add7c0901fdbb04511aef010bd7aad392f2c
DrakirL <rikard.nt.larsson@hotmail.com>
2019-03-12 13:50:43 +0100

    Added intro cutscene.
    Former-commit-id: 90d131e98ec6f3431b19d434982342ad29a564b5

---
+++ Unity/Assets/The Game/Scripts/Effects/Changing Scenes/ChangeSceneWhenDone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ChangeSceneWhenDone : ChangeScene
+{
+    VideoPlayer player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+    }
+
+    private void Update()
+    {
+        if (player.time >= player.length)
+            Change();
+    }
+}
---
+++ Unity/Assets/The Game/Scripts/Effects/Changing Scenes/ChangeSceneWithKey.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneWithKey : ChangeScene
+{
+    [SerializeField]
+    KeyCode key = KeyCode.Escape;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key))
+            Change();
+    }
+}
